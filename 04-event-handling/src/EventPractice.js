6034769c34f5c492475502c13f3c45c42bf6a5c5
qodbtn41 <qodbtn41@naver.com>
2021-02-07 23:53:43 +0900

    객체를 state로 지정하기

--- 04-event-handling/src/EventPractice.js
+++ 04-event-handling/src/EventPractice.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const onChange = (e) => {
+    console.log(form);
+    const nextForm = {
+      // 객체나 배열을 변경할떄는 전체를 복사한 다음 원하는 값만 수정한다.
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    console.log(nextForm);
+    setForm(nextForm);
+  };
   const onClick = () => {
-    alert(username + ": " + message);
-    setMessage("");
-    setUsername("");
+    alert(form.username + ": " + form.message);
+    setForm({
+      username: "",
+      message: "",
+    });
   };
   const onKeyPress = (e) => {
     if (e.key === "Enter") {
-      console.log("this : " + this);
       onClick();
     }
   };
@@ -24,16 +35,16 @@ const EventPractice = () => {
         type="text"
         name="username"
         placeholder="사용자명"
-        value={username}
-        onChange={onChangeUsername}
+        value={form.username}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
-        value={message}
-        onChange={onChangeMessage}
+        value={form.message}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
