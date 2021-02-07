394e01dac8b5e73f67e54cb20761b9f5ca899f50
qodbtn41 <qodbtn41@naver.com>
2021-02-07 22:37:26 +0900

    setState 할 때 Key값으로 변수 지정하기 & 이벤트 하나로 다 적용

--- 04-event-handling/src/EventPracticeClass.js
+++ 04-event-handling/src/EventPracticeClass.js
@@ -2,27 +2,40 @@ import React, { Component } from "react";
 
 class EventPracticeClass extends Component {
   state = {
+    username: "",
     message: "",
   };
 
   handleChange = (e) => {
     console.log(e.target.value);
-    this.setState({ message: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + ": " + this.state.message);
     this.setState({
+      username: "",
       message: "",
     });
   };
   render() {
-    const { message } = this.state;
     return (
       <div>
         <h1>이벤트 연습</h1>
-        <p>{message}</p>
-        <input type="text" name="message" placeholder="아무거나 입력해 보세요" onChange={this.handleChange} />
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
         <button onClick={this.handleClick}>확인</button>
       </div>
     );
