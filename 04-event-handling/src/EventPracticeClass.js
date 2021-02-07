db90f9ed6411cdce077b5a99c632c63f59ccf2c5
qodbtn41 <qodbtn41@naver.com>
2021-02-07 22:12:07 +0900

    onChange 이벤트 핸들링하기

--- 04-event-handling/src/EventPracticeClass.js
+++ 04-event-handling/src/EventPracticeClass.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
 
 class EventPracticeClass extends Component {
+  state = {
+    message: "",
+  };
+
   render() {
+    const { message } = this.state;
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <p>{message}</p>
         <input
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           onChange={(e) => {
             console.log(e.target.value);
+            this.setState({ message: e.target.value });
           }}
         />
+        <button
+          onClick={() => {
+            alert(message);
+            this.setState({
+              message: "",
+            });
+          }}
+        >
+          확인
+        </button>
       </div>
     );
   }
