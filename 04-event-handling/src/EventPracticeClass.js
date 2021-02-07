a81455750af23f2bea229c36737891817acdd4ca
qodbtn41 <qodbtn41@naver.com>
2021-02-07 22:22:06 +0900

    임의 메서드 & 생성자의 this 바인딩

--- 04-event-handling/src/EventPracticeClass.js
+++ 04-event-handling/src/EventPracticeClass.js
@@ -5,31 +5,31 @@ class EventPracticeClass extends Component {
     message: "",
   };
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState({ message: e.target.value });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
   render() {
     const { message } = this.state;
     return (
       <div>
         <h1>이벤트 연습</h1>
         <p>{message}</p>
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요"
-          onChange={(e) => {
-            console.log(e.target.value);
-            this.setState({ message: e.target.value });
-          }}
-        />
-        <button
-          onClick={() => {
-            alert(message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
+        <input type="text" name="message" placeholder="아무거나 입력해 보세요" onChange={this.handleChange} />
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
