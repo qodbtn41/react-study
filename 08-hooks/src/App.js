196ea69ebcda0a1754226ed05435db7399009f08
qodbtn41 <qodbtn41@naver.com>
2021-02-15 04:23:44 +0900

    useEffect 함수

--- 08-hooks/src/App.js
+++ 08-hooks/src/App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
 import Info from "./Info";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
   // return <Counter />;
-  return <Info />;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
--- 08-hooks/src/Info.js
+++ 08-hooks/src/Info.js
@@ -1,8 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("effect. name: ", name);
+    return () => {
+      console.log("cleanup. name: ", name);
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("effect. name: ", name);
+  //   return () => {
+  //     console.log("cleanup. name: ", name);
+  //   };
+  // }, [name]);
+
+  // useEffect(() => {
+  //   console.log("렌더링이 완료되었습니다.");
+  //   console.log({
+  //     name,
+  //     nickname,
+  //   });
+  // });
+
+  // useEffect(() => {
+  //   console.log("마운트될 때만 실행됩니다.");
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log("name 값이 업데이트될 때만 실행됩니다. name: ", { name });
+  // }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
