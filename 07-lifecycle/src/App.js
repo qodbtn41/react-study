6c58dd0a8fc4a9d759f718dfc15470bbef206621
qodbtn41 <qodbtn41@naver.com>
2021-02-14 23:09:14 +0900

    componentDidCatch로 child 컴포넌트 에러 처리

--- 07-lifecycle/src/App.js
+++ 07-lifecycle/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import LifeCycleSample from "./LifeCycleSample";
 import { Component } from "react";
+import ErrorBoundary from "./ErrorBoundary";
 
 function getRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -20,7 +21,9 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color} />
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
