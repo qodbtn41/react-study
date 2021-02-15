6f8336671fa9c9c725a00a8e67f6e2bd15f5ee25
qodbtn41 <qodbtn41@naver.com>
2021-02-16 02:21:57 +0900

    useCallback

--- 08-hooks/src/Average.js
+++ 08-hooks/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중..");
@@ -11,15 +11,20 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
+  }, []); // 컴포넌트가 처음 렌더링 될 때만 함수 생성
 
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+    },
+    [number, list] // number 혹은 list가 바뀌었을 때만 함수 생성
+  );
+
+  const avg = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
@@ -31,7 +36,7 @@ const Average = () => {
         ))}
       </ul>
       <div>
-        <b>평균값:</b> {getAverage(list)}
+        <b>평균값:</b> {avg}
       </div>
     </div>
   );
