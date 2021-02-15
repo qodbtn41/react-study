be65c1bde5d4b12b88c3f55e5cb5acff72e5763c
qodbtn41 <qodbtn41@naver.com>
2021-02-16 01:18:05 +0900

    Counter에 리듀서 사용

--- 08-hooks/src/App.js
+++ 08-hooks/src/App.js
@@ -4,22 +4,22 @@ import Counter from "./Counter";
 import Info from "./Info";
 
 function App() {
-  const [visible, setVisible] = useState(false);
+  // const [visible, setVisible] = useState(false);
 
-  // return <Counter />;
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Counter />;
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
 }
 
 export default App;
