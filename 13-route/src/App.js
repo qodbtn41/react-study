bc14f89f4f2e003db1c485694603fae72cd8449b
qodbtn41 <qodbtn41@naver.com>
2021-03-24 02:06:03 +0900

    매개변수 전달

--- 13-route/src/App.js
+++ 13-route/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import { Route } from "react-router";
 import About from "./About";
 import { Link } from "react-router-dom";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -15,10 +16,17 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
