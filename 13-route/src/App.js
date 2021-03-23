3697f69f7b7b655b69d4d48e9476d75ca995bc3a
qodbtn41 <qodbtn41@naver.com>
2021-03-24 01:54:10 +0900

    Link

--- 13-route/src/App.js
+++ 13-route/src/App.js
@@ -3,10 +3,20 @@ import "./App.css";
 import Home from "./Home";
 import { Route } from "react-router";
 import About from "./About";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
