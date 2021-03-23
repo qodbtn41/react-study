5c52aca0a1cbceae99db8296202ece2404356331
qodbtn41 <qodbtn41@naver.com>
2021-03-24 01:50:38 +0900

    BrowserRoute, Route, exact

--- 13-route/src/App.js
+++ 13-route/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import { Route } from "react-router";
+import About from "./About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
