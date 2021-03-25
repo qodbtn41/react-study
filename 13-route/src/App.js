47b2a9b0c9fd7622e7b2a5235814349629f306bf
qodbtn41 <qodbtn41@naver.com>
2021-03-25 23:41:12 +0900

    History

--- 13-route/src/App.js
+++ 13-route/src/App.js
@@ -5,6 +5,7 @@ import { Route } from "react-router";
 import About from "./About";
 import { Link } from "react-router-dom";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -19,11 +20,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
