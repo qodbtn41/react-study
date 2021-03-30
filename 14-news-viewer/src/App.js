2b76f9ab67e68e4d23d12c56007d0bab01b3001a
qodbtn41 <qodbtn41@naver.com>
2021-03-31 02:34:37 +0900

    14.5 뉴스 정보 표시

--- 14-news-viewer/src/App.js
+++ 14-news-viewer/src/App.js
@@ -1,32 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=2b9d5dc62ab042748713468e4f7aa8f9',
       );
       setData(response.data);
     } catch (e) {
       console.log(e);
     }
   };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
+// 2b9d5dc62ab042748713468e4f7aa8f9
